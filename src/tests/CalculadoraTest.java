a0c7bd06877b15f5c76a8e71cae2a26d636e59ec
Wesley Farias <wesleysousukee@gmail.com>
2022-05-25 13:24:36 -0300

    testando o retorno da soma

--- src/tests/CalculadoraTest.java
+++ src/tests/CalculadoraTest.java
@@ -6,10 +6,19 @@ import org.junit.jupiter.api.Test;
 
 
 public class CalculadoraTest {
+    Calculadora calculadora = new Calculadora();
+
     @Test
+    // Soma dois números positivos
     public void somaDoisNumerosPositivos() {
-        Calculadora calculadora = new Calculadora();
-        int somaPositivo = calculadora.somar(10, 10);
-        Assertions.assertEquals(20, somaPositivo);
+        int soma = calculadora.somar(10, 10);
+        Assertions.assertEquals(20, soma);
+    }
+
+    @Test
+    // Soma um número positivo com um número negativo
+    public void somaUmNumeroPositivoEumNegativo() {
+        int soma = calculadora.somar(10, -20);
+        Assertions.assertEquals(-10, soma);
     }
 }
