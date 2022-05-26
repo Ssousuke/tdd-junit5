0e4a9e66300cd879e3125de8ff0677f5ad41d5ba
Ssousuke <wesleysousukee@gmail.com>
2022-05-25 23:15:02 -0300

    tdd

--- src/tests/CalculadoraTest.java
+++ src/tests/CalculadoraTest.java
@@ -6,10 +6,28 @@ import org.junit.jupiter.api.Test;
 
 
 public class CalculadoraTest {
+    Calculadora calculadora = new Calculadora();
     @Test
     public void somaDoisNumerosPositivos() {
-        Calculadora calculadora = new Calculadora();
         int somaPositivo = calculadora.somar(10, 10);
         Assertions.assertEquals(20, somaPositivo);
+    }
+
+    @Test
+    public void somaUmNumeroPositivoEumNegativo() {
+        int somaPositivo = calculadora.somar(10, -20);
+        Assertions.assertEquals(-10, somaPositivo);
+    }
+
+    @Test
+    public void multiplicaDoisNumerosPositivos() {
+        int multiplica = calculadora.multiplica(2, 10);
+        Assertions.assertEquals(20, multiplica);
+    }
+
+    @Test
+    public void multiplicaUmNumeroPositivoEumNumeroNegativo() {
+        int multiplica = calculadora.multiplica(2, -10);
+        Assertions.assertEquals(-20, multiplica);
     }
 }
