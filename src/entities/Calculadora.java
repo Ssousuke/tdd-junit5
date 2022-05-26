ef8dc30d1204b88b3e88201e3e81dfac7cb58597
Ssousuke <wesleysousukee@gmail.com>
2022-05-25 23:08:34 -0300

    tdd

--- src/entities/Calculadora.java
+++ src/entities/Calculadora.java
@@ -4,4 +4,8 @@ public class Calculadora {
     public int somar(int a, int b) {
         return a + b;
     }
+
+    public int multiplica(int a, int b) {
+        return a * b;
+    }
 }
--- src/tests/CalculadoraTest.java
+++ src/tests/CalculadoraTest.java
@@ -6,19 +6,10 @@ import org.junit.jupiter.api.Test;
 
 
 public class CalculadoraTest {
-    Calculadora calculadora = new Calculadora();
-
     @Test
-    // Soma dois números positivos
     public void somaDoisNumerosPositivos() {
-        int soma = calculadora.somar(10, 10);
-        Assertions.assertEquals(20, soma);
-    }
-
-    @Test
-    // Soma um número positivo com um número negativo
-    public void somaUmNumeroPositivoEumNegativo() {
-        int soma = calculadora.somar(10, -20);
-        Assertions.assertEquals(-10, soma);
+        Calculadora calculadora = new Calculadora();
+        int somaPositivo = calculadora.somar(10, 10);
+        Assertions.assertEquals(20, somaPositivo);
     }
 }
