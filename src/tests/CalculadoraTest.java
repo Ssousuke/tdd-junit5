003f150f2bbafb070dc56c57e2a05ead9d37e5e1
Wesley Farias <wesleysousukee@gmail.com>
2022-05-26 21:23:30 -0300

    tdd

--- src/tests/CalculadoraTest.java
+++ src/tests/CalculadoraTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
     Calculadora calculadora = new Calculadora();
+
     @Test
     public void somaDoisNumerosPositivos() {
         int somaPositivo = calculadora.somar(10, 10);
@@ -29,5 +30,23 @@ public class CalculadoraTest {
     public void multiplicaUmNumeroPositivoEumNumeroNegativo() {
         int multiplica = calculadora.multiplica(2, -10);
         Assertions.assertEquals(-20, multiplica);
+    }
+
+    @Test
+    public void divideDoisNumerosInteiros() {
+        int divide = calculadora.divisao(10, 2);
+        Assertions.assertEquals(5, divide);
+    }
+
+    @Test
+    public void divideUmNumeroPositivoEumNegativo() {
+        int divide = calculadora.divisao(20, -10);
+        Assertions.assertEquals(-2, divide);
+    }
+
+    @Test
+    public void subtraiUmNumeroPositivoEumNegativo() {
+        int divide = calculadora.subtracao(2, -1);
+        Assertions.assertEquals(3, divide);
     }
 }
